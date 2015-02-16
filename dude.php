67c312b30e143f025b40a7da9be923e6dd4ccc3b
kosvrouvas <kosvrouvas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-16 12:31:14 +0000

    The Dude, dude.
    git-svn-id: https://plugins.svn.wordpress.org/the-dude/trunk@1091304 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dude.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @package The_Dude
+ * @version 1.0
+ */
+/*
+Plugin Name: The Dude
+Plugin URI:
+Description: That, or His Dudeness… Duder… or El Duderino, if, you know, you're not into the whole brevity thing.
+Author: Kostas Vrouvas
+Version: 1.6
+Author URI: http://kosvrouvas.com/
+*/
+
+function the_dude_get_lyric() {
+	/** Quotes from the movie */
+	$lyrics = "That rug really tied the room together.
+You're Mr. Lebowski. I'm the Dude.
+That, or His Dudeness… Duder… or El Duderino, if, you know, you're not into the whole brevity thing.
+Obviously you're not a golfer.
+Yeah, well. The Dude abides.
+Is this a... what day is this?
+Uh, I'm just gonna go find a cash machine.
+Well, they finally did it. They killed my fucking car.
+Walter, he peed on my rug!
+Mr. Treehorn treats objects like women, man.
+I! The Royal 'we'! You know, the editorial...
+Oh, the usual. I bowl. Drive around. The occasional acid flashback.
+Fuckin' A.
+Hey, careful, man, there's a beverage here!
+It's uh... uh... it's down there somewhere, let me take another look.
+That's a great plan, Walter. That's fuckin' ingenious, if I understand it correctly. It's a Swiss fuckin' watch.
+Mind if I do a J?
+This aggression will not stand, man.
+Uzi?
+At least I'm housebroken.
+I hate the fuckin' Eagles man.
+Also, my rug was stolen.
+I don't see any connection to Vietnam, Walter.
+And, you know, he's got emotional problems, man.
+Jesus, man, could you change the channel?
+Yeah,well, that's just, like, your opinion, man.
+She probably kidnapped herself.
+You human paraquat!
+Nice marmont.";
+
+	// Here we split it into lines
+	$lyrics = explode( "\n", $lyrics );
+
+	// And then randomly choose a line
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+// This just echoes the chosen line, we'll position it later
+function the_dude() {
+	$chosen = the_dude_get_lyric();
+	echo "<p id='dude'>$chosen</p>";
+}
+
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'the_dude' );
+
+// We need some CSS to position the paragraph
+function dude_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
+	echo "
+	<style type='text/css'>
+	#dude {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 11px;
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'dude_css' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== The Dude ===
+Contributors: kosvrouvas
+Tags: hello, admin area, humor, the dude, his dudeness, el duderino, fun
+Requires at least: 3.0
+Stable tag: 1.0
+Tested up to: 4.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+That, or His Dudeness… Duder… or El Duderino, if, you know, you're not into the whole brevity thing.
+
+== Description ==
+
+That, or His Dudeness… Duder… or El Duderino, if, you know, you're not into the whole brevity thing.
+
+When activated you will randomly see a quote from "The Big Lebowski" in the upper right of your admin screen on every page, h/t to Matt Mullenberg for the original code.
+
+== Changelog ==
+
+= 1.0 =
+- First release
